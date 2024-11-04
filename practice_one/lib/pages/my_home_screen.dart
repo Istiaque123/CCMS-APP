f12b681c7f37ec2465948454d420e3772ae1d7d4
Md. Istiaque Hossain <md.istiaquehossain@Mds-MacBook-Air.local>
2024-11-04 15:27:51 +0600

    Create 1st two pages

---
+++ practice_one/lib/pages/my_home_screen.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class MyHomeScreen extends StatefulWidget {
+  const MyHomeScreen({super.key});
+
+  @override
+  State<MyHomeScreen> createState() => _MyHomeScreenState();
+}
+
+class _MyHomeScreenState extends State<MyHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: bodyWeiget(),
+      ),
+    );
+  }
+
+  Widget bodyWeiget(){
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset(
+                "assets/images/spalsh_logo.png",
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          const SizedBox(height: 150,),
+          const Text(
+            'জাতীয় ভোক্তা অধিকার সংরক্ষন অধিদপ্তর',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.w600,
+              color: Colors.black
+            ),
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            ElevatedButton(
+              onPressed: (){}, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xff15803D),
+                foregroundColor: Colors.white,
+                 minimumSize: const Size(350, 0),
+                 padding: const EdgeInsets.symmetric(vertical: 12)
+
+              ),
+
+              child: const Text(
+                "রেজিস্ট্রেশন",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+                )
+              ),
+              const SizedBox(height: 20,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                        
+                      )
+                    ),
+                  ),
+                   
+                  const SizedBox(width: 15,),
+                  const Text(
+                    'অথবা',
+                    style: TextStyle(
+                      fontSize: 15
+                    ),
+                    ),
+                  const SizedBox(width: 15,),
+                   Container(
+                    width: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                        
+                      )
+                    ),
+                  ),
+
+                ],
+              ),
+              const SizedBox(height: 20,),
+              const Text(
+                'ইতঃমধ্যে রেজিস্ট্রেশন করেছেন?',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black
+                ),
+              ),
+
+              const SizedBox(height: 20,),
+              ElevatedButton(
+              onPressed: (){}, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor:  Colors.white,
+                foregroundColor: Colors.black,
+                 minimumSize: const Size(350, 0),
+                 padding: const EdgeInsets.symmetric(vertical: 12),
+                 
+                 side: BorderSide(
+                  width: 1,
+                  color: Colors.black
+                 )
+
+              ),
+
+              child: const Text(
+                "রেজিস্ট্রেশন",
+                style: TextStyle(
+                  fontSize: 20
+                ),
+                )
+              ),
+              const SizedBox(height: 20,),
+        ],
+      ),
+    );
+  }
+}
---
+++ practice_one/lib/pages/spalash_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:practice_one/pages/my_home_screen.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: bodyWidget(context),
+      ),
+    );
+  }
+
+  // Body Widget
+  Widget bodyWidget(var context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: const BoxDecoration(
+        color: Color(0xff15803D),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 200)),
+          const Text(
+            "জাতীয় ভোক্তা অধিকার সংরক্ষন অধিদপ্তরে অফিসিয়াল এপ্লিকেশনে আপনাকে স্বাগতম",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 36,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            height: 200,
+            child: Image.asset(
+              "assets/images/spalsh_logo.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            "CCMS",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 36,
+              color: Colors.white,
+            ),
+          ),
+          const Spacer(), 
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context)=> const MyHomeScreen()));
+            },
+            style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                minimumSize: const Size(double.infinity, 0),
+                backgroundColor: Colors.white,
+                elevation: 10,
+                shadowColor: Colors.black.withOpacity(0.7)),
+
+            child: const Text(
+              'শুরু করুন',
+              style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  color: Colors.black),
+            ),
+          ),
+          const SizedBox(height: 20), 
+        ],
+      ),
+    );
+  }
+}
