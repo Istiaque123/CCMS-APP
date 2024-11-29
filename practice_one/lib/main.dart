176d37b17e6aacc72575fb9ee0623d8848450271
Istiaque123 <istiaqueremon@gmail.com>
2024-11-29 12:37:36 +0600

    complete regestration process

--- practice_one/lib/main.dart
+++ practice_one/lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:practice_one/common/constraints/media_size_pad.dart';
-import 'package:practice_one/pages/address_screen.dart';
-import 'package:practice_one/pages/inside%20app/views/home_page_screen.dart';
-import 'package:practice_one/pages/log_reg_select_screen.dart';
-import 'package:practice_one/pages/opt_reg_screen.dart';
-import 'package:practice_one/pages/password_create_screen.dart';
-import 'package:practice_one/pages/personal_info_check.dart';
-import 'package:practice_one/pages/personal_info_id_scrren.dart';
-import 'package:practice_one/pages/personal_info_name_screen.dart';
+
 import 'package:practice_one/pages/phone_num_reg_screen.dart';
-import 'package:practice_one/pages/profession_screen.dart';
-import 'package:practice_one/pages/profile_img_take_screen.dart';
-
-
-
-import 'package:practice_one/pages/spalash_screen.dart';
-
 void main() {
   runApp( const ProviderScope(child: MyApp()));
 }
